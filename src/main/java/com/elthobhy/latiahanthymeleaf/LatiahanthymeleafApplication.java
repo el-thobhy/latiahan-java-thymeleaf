9a98de94e78fb2adae6104c315327762b5e65915
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-05-31 08:51:05 +0700

    first init

---
+++ src/main/java/com/elthobhy/latiahanthymeleaf/LatiahanthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.elthobhy.latiahanthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatiahanthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatiahanthymeleafApplication.class, args);
+	}
+
+}
